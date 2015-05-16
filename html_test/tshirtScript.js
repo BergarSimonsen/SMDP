87d0cc401c2b4c82a8a4216390bdde015ab5e4ca
Aleksandra Korach <sgsbox90@gmail.com>
2015-05-16 16:03:07 +0200

    Shirt dummy with listBox parameter and saving sample data

--- html_test/tshirtScript.js
+++ html_test/tshirtScript.js
@@ -1,15 +1,18 @@
 $(document).ready(function(){	
-	var $ColourValues = ["RED", "BLACK", "BLUE"];
+	var $ColourValues = ["Red", "Black", "Blue"];
 	$("#Colour").jqxComboBox({ source: $ColourValues, width: '200px', height: '25px',});
 	
 	var $SizeValues = [1, 2, 3, 4, 5];
 	$("#Size").jqxComboBox({ source: $SizeValues, width: '200px', height: '25px',});
 	
-	var $PrintColourValues = ["RED", "BLACK", "BLUE"];
+	var $PrintColourValues = ["Red", "Black", "Blue"];
 	$("#PrintColour").jqxComboBox({ source: $PrintColourValues, width: '200px', height: '25px',});
 	
-	var $PrintShapeValues = ["ROUND", "SQUARE", "TRIANGULAR"];
-	$("#PrintShape").jqxComboBox({ source: $PrintShapeValues, width: '200px', height: '25px',});	
+	var $PrintShapeValues = ["Round", "Square", "Triangular"];
+	$("#PrintShape").jqxComboBox({ source: $PrintShapeValues, width: '200px', height: '25px',});
+	
+	var $PrintPlacementValues = ["Front", "Back", "LeftSleeve", "RightSleeve"];
+	$("#PrintPlacement").jqxListBox({ source: $PrintPlacementValues, width: '200px', height: '150px', multiple: true});		
 	
 	function checkConstraints() {		
 		var valid = "";
@@ -22,7 +25,7 @@ $(document).ready(function(){
 		if(valid === "") {
 			if(!($("#Colour").jqxComboBox('getSelectedItem').value != $("#PrintColour").jqxComboBox('getSelectedItem').value)) 
 				valid += "Invalid constraint: " + "(Colour != PrintColor) \n";
-			if(!((($("#Colour").jqxComboBox('getSelectedItem').value != "RED") || (($("#Colour").jqxComboBox('getSelectedItem').value === "RED") 
+			if(!((($("#Colour").jqxComboBox('getSelectedItem').value != "Red") || (($("#Colour").jqxComboBox('getSelectedItem').value === "Red") 
 				&& ($("#Size").jqxComboBox('getSelectedItem').value > 3))))) 
 				valid += "Invalid constraint: " + "((Color != String.RED) || ((Color == String.RED) && (ShirtSize > 3))) \n";
 		}
@@ -30,19 +33,48 @@ $(document).ready(function(){
 		return valid;
 	}
 	
-	function save() {
-		alert("Saving!");
-	}
+	function getText() {
+		return "abc";
+	}	
 
 	$('#submitButton').click(function(){
 		var validationMsg = checkConstraints();
 		
-		if(validationMsg.length === 0){
+		if(validationMsg.length === 0)
 			save();
-			
-		}
-		else {
+		else
 			alert(validationMsg + "\n Please correct the values!");
-		}		
 	});
+	
+	function save() {
+		var textToWrite = getText();
+		var textFileAsBlob = new Blob([textToWrite], {type:'text/plain'});
+		var fileNameToSaveAs = "config.txt";
+
+		var downloadLink = document.createElement("a");
+		downloadLink.download = fileNameToSaveAs;
+		downloadLink.innerHTML = "Download File";
+		if (window.webkitURL != null) {
+			// Chrome allows the link to be clicked
+			// without actually adding it to the DOM.
+			downloadLink.href = window.webkitURL.createObjectURL(textFileAsBlob);
+		}
+		else
+		{
+			// Firefox requires the link to be added to the DOM
+			// before it can be clicked.
+			downloadLink.href = window.URL.createObjectURL(textFileAsBlob);
+			downloadLink.onclick = destroyClickedElement;
+			downloadLink.style.display = "none";
+			document.body.appendChild(downloadLink);
+		}
+
+		downloadLink.click();
+		alert("Saved!");
+	}
+	
+	function destroyClickedElement(event)
+	{
+		document.body.removeChild(event.target);
+	}	
 });
